619d931b11b7318fd20af85f405f79841378dc04
elnaqah <eng.ahmed.medhat@gmail.com>
2013-05-23 01:49:01 +0200

    Initial Commit

---
+++ MSD/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MSD
+//
+//  Created by AhmedElnaqah on 5/23/13.
+//  Copyright (c) 2013 elnaqah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
